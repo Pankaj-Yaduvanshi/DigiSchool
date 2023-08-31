e3a3bf937b7851c09ad917e32fc10265a96ac180
Pankaj-Yaduvanshi <110768098+Pankaj-Yaduvanshi@users.noreply.github.com>
2023-08-31 23:08:38 +0530

    for questionnaire

--- src/main/java/com/smart/repository/QuestionRepository.java
+++ src/main/java/com/smart/repository/QuestionRepository.java
@@ -1,7 +1,2 @@
-package com.smart.repository;
-
-import com.smart.entities.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-     }
+package com.smart.repository;public class QuestionRepository {
+}
