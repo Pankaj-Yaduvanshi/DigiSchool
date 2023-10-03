6d98d9763fb33fafb2c99c0c6bf7ceb7cb3e63e0
Pankaj-Yaduvanshi <110768098+Pankaj-Yaduvanshi@users.noreply.github.com>
2023-10-03 20:47:18 +0530

    After exam page

--- src/main/java/com/DigiSchool/controller/AdminController.java
+++ src/main/java/com/DigiSchool/controller/AdminController.java
@@ -47,22 +47,9 @@ public class AdminController {
     }
 //    handler for registering user
     @RequestMapping(value = "/do_register", method = RequestMethod.POST)
-    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result1,
-                               @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
-                               HttpSession session) {
-
+    public String registerUser(@Valid @ModelAttribute("user") User user, Model model, HttpSession session) {
         try {
-            if (!agreement) {
-                System.out.println("You have not agreed the terms and conditions");
-                throw new Exception("You have not agreed the terms and conditions");
-            }
-            if (result1.hasErrors()) {
-                System.out.println("ERROR " + result1.toString());
-                model.addAttribute("user", user);
-                return "admin/add_user";
-            }
             user.setRole(Role.REGULAR);
-            user.setEnabled(true);
             user.setImageUrl("default.png");
             user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
             User result = this.userRepository.save(user);
--- src/main/java/com/DigiSchool/controller/RegularController.java
+++ src/main/java/com/DigiSchool/controller/RegularController.java
@@ -1,10 +1,16 @@
 package com.DigiSchool.controller;
 import java.security.Principal;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.DigiSchool.Helper.Message;
 import com.DigiSchool.Model.Assignment;
+import com.DigiSchool.Model.Question;
+import com.DigiSchool.Model.Result;
 import com.DigiSchool.Repository.AssignmentRepository;
+import com.DigiSchool.Repository.QuestionRepository;
+import com.DigiSchool.Repository.ResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,6 +23,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.DigiSchool.Repository.UserRepository;
 import com.DigiSchool.Model.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/a/regular")
@@ -25,6 +34,10 @@ public class RegularController {
 	private UserRepository userRepository;
 	@Autowired
 	private AssignmentRepository assignmentRepository;
+	@Autowired
+	private ResultRepository resultRepository;
+	@Autowired
+	private QuestionRepository questionRepository;
 
 	// method for adding common data to response
 	@ModelAttribute
@@ -63,23 +76,40 @@ public class RegularController {
 		model.addAttribute("assignment", assignment);
 		return "regular/exam-page";
 	}
-//	@RequestMapping("/{aId}/submit-exam")
-//	public String submitExam(Model model, Principal principal, @PathVariable("aId") Integer aId) {
-//		try {
-//			Optional<Assignment> assignmentOptional = this.assignmentRepository.findById(aId);
-//			Assignment assignment = assignmentOptional.get();
-//			question.setAssignment(assignment);
-//			questionRepository.save(question);
-//			m.addAttribute("assignment", assignment);
-////			message success.......
-//			session.setAttribute("message", new Message("Your Question added successfully!!!!!", "success"));
-//		} catch (Exception e) {
-//			System.out.println("ERROR " + e.getMessage());
-//			e.printStackTrace();
-////          message error
-//			session.setAttribute("message", new Message("Something went wrong !! Try again..", "danger"));
-//		}
-//		return "trainer/assignment_detail";
-//	}
-//
+	@RequestMapping("/{aId}/submit-exam")
+	public String submitExam(@RequestParam Map<String, String> formData, Principal principal, @PathVariable("aId") Integer aId, HttpSession session){
+		try {
+				Optional<Assignment> assignmentOptional = this.assignmentRepository.findById(aId);
+				Assignment assignment = assignmentOptional.get();
+				List<Question> questionsList = assignment.getQuestions();
+				int numberOfQuestions = questionsList.size();
+				int score =0;
+				for (Map.Entry<String,String> entry : formData.entrySet())
+				{
+					int index = Integer.parseInt(entry.getKey());
+					if(entry.getValue().equals(questionRepository.findById(index).get().getAnswer()))
+						score++;
+				}
+				if(score>=numberOfQuestions*0.8)
+				{
+					User user = userRepository.getUserByUserName(principal.getName());
+					Result result = new Result();
+					result.setUser(user);
+					result.setAssignment(assignment);
+					result.setResult(formData.toString());
+					resultRepository.save(result);
+		//			message success.......
+					session.setAttribute("message", new Message("Congratulations you passed the exam!!!!!", "success"));
+				}
+				else
+					session.setAttribute("message", new Message("Sorry you did not pass the exam!!!!!", "danger"));
+
+		} catch (Exception e) {
+			System.out.println("ERROR " + e.getMessage());
+			e.printStackTrace();
+			// message error
+			session.setAttribute("message", new Message("Something went wrong !! Try again..", "danger"));
+		}
+		return "redirect:/"+ "a/regular/"+aId+"/exam";
+	}
 }
